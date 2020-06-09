22d2d60880f30ed4996473f7f9e0b664cfca90b3
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-06-09 14:33:42 -0500

    Add initial code

---
+++ your-project/hangman.py
@@ -0,0 +1,15 @@
+# Hangman game
+
+import random
+
+WORDLIST_FILENAME = 'words.txt'
+
+def load_words():
+    '''
+    Returns a list of valid words.
+    '''
+    print('Loading word list from file...')
+
+if __name__ == '__main__':
+    word_list = load_words()
+    
