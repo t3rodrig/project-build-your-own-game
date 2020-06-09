f760e6663a22cddaf362a3667fc53e99f04c7ef6
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-06-09 15:56:27 -0500

    Create load_words function

--- your-project/hangman.py
+++ your-project/hangman.py
@@ -2,14 +2,22 @@
 
 import random
 
-WORDLIST_FILENAME = 'words.txt'
+WORDLIST_FILENAME = 'wordlist.txt'
 
 def load_words():
     '''
     Returns a list of valid words.
     '''
     print('Loading word list from file...')
+    input_File = open(WORDLIST_FILENAME, 'r')
+    content = input_File.read()
+    word_list = content.split('\n')
+    try:
+        word_list.remove('')
+    except ValueError:
+        print('There is no empty string')
+    print(f'\t {len(word_list)} words loaded.') 
+    return word_list
 
 if __name__ == '__main__':
     word_list = load_words()
-    
