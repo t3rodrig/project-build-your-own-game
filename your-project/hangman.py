64856883f5607bf7109e039d7b60dd8795dcd722
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-06-10 17:19:18 -0500

    Complete hangman function

--- your-project/hangman.py
+++ your-project/hangman.py
@@ -73,11 +73,57 @@ def get_available_letters(letters_guessed):
     available = sorted(available)
     return ''.join(available)
 
-def hangman():
-    pass
+def hangman(secret_word):
+    '''
+    Starts up an interactive game of Hangman between the user
+    and the computer.
+
+    Args:
+        secret_word (str): the word the user is guessing.
+    '''
+    print('Welcome to the game, Hangman!')
+    print(f'I am thinking of a word that is {len(secret_word)} letters long.')
+    print('-------------')
+
+    mistakes_made = 0
+    max_guess = 8
+
+    letters_guessed = []
+    is_winner = False
+    is_done = False
+
+    while not is_done:
+        print(f'You have {max_guess - mistakes_made} guesses left.')
+        available_letters = get_available_letters(letters_guessed)
+        print(f'Available letters: {available_letters}')
+        guess = input('Please guess a letter: ').lower()
+
+        if guess in letters_guessed:
+            print("Oops! You've already guessed that letter:", end=' ')
+            print(get_guessed_word(secret_word, letters_guessed))
+        elif guess in secret_word:
+            letters_guessed.append(guess)
+            print('Good guess:', end=' ')
+            print(get_guessed_word(secret_word, letters_guessed))
+            if is_word_guessed(secret_word, letters_guessed):
+                is_winner = True
+                is_done = True
+        else:
+            letters_guessed.append(guess)
+            print('Oops! That letter is not in my word:', end=' ')
+            print(get_guessed_word(secret_word, letters_guessed))
+            mistakes_made += 1
+            if mistakes_made == max_guess:
+                is_done = True
+        
+        print('-------------')
+    
+    if is_winner:
+        print('Congratulations, you won!')
+    else:
+        print(f'Sorry, you ran out of guesses. The word was {secret_word}')
 
 if __name__ == '__main__':
-    #word_list = load_words()
-    #secret_word = choose_word(word_list).lower()
-    secret_word = 'thermodynamics'
-    print(secret_word)
+    word_list = load_words()
+    secret_word = choose_word(word_list).lower()
+    hangman(secret_word)
